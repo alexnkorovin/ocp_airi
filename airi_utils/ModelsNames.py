ff355f60803a7fa11698013f92f79a0aa04ce65c
kesha_humonen <g1410ish@gmail.com>
2021-10-19 15:55:57 +0300

    terminal instead of ipybn

---
+++ airi_utils/ModelsNames.py
@@ -0,0 +1,18 @@
+models_names = {
+				'spinconv_with_embeds' : 'spinconv_with_embeds.py',
+				'spinconv_double_angles' : 'spinconv_double_angles',
+				'spinconv' : 'spinconv.py',
+				'spinconv_with_embeds_single' : 'spinconv_with_embeds_single.py',
+				'spinconv_with_solid_angles' : 'spinconv_with_solid_angles.py',
+				'spinconv_with_radii' : 'spinconv_with_radii.py',
+				'spinconv_with_tag' : 'spinconv_with_tag.py',
+				'spinconv_with_val_vor_tag' : 'spinconv_with_val_vor_tag.py',
+				'SpinConv_custom_embeds' : 'spinconv_with_embeds.py',
+				'SpinConv_double_angles' : 'spinconv_double_angles',
+				'SpinConv_identical' : 'spinconv.py',
+				'SpinConv_identical_double_custom_embeds' : 'spinconv_with_embeds_single.py',
+				'SpinConv_identical_single_custom_embeds' : 'spinconv_with_embeds_single.py',
+				'SpinConv_solid_angles' : 'spinconv_with_solid_angles.py',
+				'SpinConv_with_radii' : 'spinconv_with_radii.py',
+				'SpinConv_with_val_vor_tag' : 'spinconv_with_val_vor_tag.py'
+				}
